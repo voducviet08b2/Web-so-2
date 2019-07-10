0e83e4fef6d9162b0d6562413a6f6f12cd42d0b9
= <=>
2019-07-10 10:26:54 +0700

    first-commit

---
+++ 1.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", function() {
+    var container = document.querySelectorAll(".Container");
+    console.log(container[1].offsetTop);
+
+
+    document.addEventListener("scroll", function() {
+        for (i = 0; i < container.length; i++) {
+            if (window.pageYOffset >= container[i].offsetTop - 600) {
+                container[i].classList.add("up-effect");
+            }
+        }
+    })
+})
